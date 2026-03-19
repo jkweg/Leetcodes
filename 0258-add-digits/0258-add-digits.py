e31a2eacb2b0d65d3a011d76a926d0de2e840924
Jakub Węgrzyniak <jakub.wegrzyniak1239@gmail.com>
2026-03-19 20:17:35 +0100

    Time: 0 ms (100%), Space: 12.3 MB (77.63%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num >= 10:
+            res = 0
+            while num > 0:
+                res += num%10
+                num = num // 10
+            num = res
+        return num
